a4233993c38f1563ee5a4c404114c2a8641965e6
Bigus Dickus <you@example.com>
2021-03-24 21:03:23 +0200

    initial commit

---
+++ Graph.py
@@ -0,0 +1,291 @@
+import main
+import bisect
+from operator import itemgetter
+
+
+average_cost_per_road = 0
+
+
+class Graph:
+
+    def __init__(self, graph_dict, p1, p2, p3):
+        self.graph_dict = graph_dict
+        self.p1 = p1
+        self.p2 = p2
+        self.p3 = p3
+
+
+    # getter method 
+    def get_p1(self): 
+        return self.p1
+
+    def get_p2(self): 
+        return self.p2
+
+    def get_p3(self): 
+        return self.p3
+      
+    # setter method 
+    def set_p1(self, x): 
+        self.p1 = x 
+
+    def set_p2(self, x): 
+        self.p2 = x 
+
+    def set_p3(self, x): 
+        self.p3 = x 
+
+    def read_graph(self, file):
+
+        average_cost_per_road = 0
+        read_graph_flag = False
+        predictions_line = 0
+        actual_traffic_line = 0
+        roads_count = 0
+
+        for number, line in enumerate(file):
+
+            if(line.startswith("<Source>")):
+
+                source = main.remove_tags(line)
+
+            elif(line.startswith("<Destination>")):
+
+                destination = main.remove_tags(line)
+
+            elif(line == "<Roads>\n"):
+
+                read_graph_flag = True
+
+            elif(line == "</Roads>\n"):
+
+                read_graph_flag = False
+
+            elif(read_graph_flag is True):
+
+                roads_count += 1
+                graph_array = line.split(';' ' ', 4)
+                average_cost_per_road += int(graph_array[3])
+                self.populate_graph(graph_array[0], graph_array[1],
+                                    graph_array[2], graph_array[3].rstrip())
+
+            elif(line == "<ActualTrafficPerDay>\n"):
+
+                actual_traffic_line = number + 1
+
+            elif(line == "<Predictions>\n"):
+
+                predictions_line = number + 1
+
+        average_cost_per_road = average_cost_per_road / roads_count
+
+        return source, destination, actual_traffic_line, predictions_line, roads_count, average_cost_per_road
+
+
+    def calculate_cost_bfs(self, path):
+
+        cost_per_road = []
+        road_path = []
+        total_cost = 0
+
+        pipis = 0
+        sifis = 1
+        while sifis <= len(path) - 1:
+
+            if path[sifis] in self.graph_dict[path[pipis]]:
+
+                temp = min(self.graph_dict[path[pipis]][path[sifis]].items(), key=itemgetter(1))
+                cost_per_road.append(int(temp[1]))
+                road_path.append(temp[0])
+
+            else:
+
+                pipis += 1
+
+                while path[sifis] not in self.graph_dict[path[pipis]]:
+
+                    pipis += 1
+
+                temp = min(self.graph_dict[path[pipis]][path[sifis]].items(), key=itemgetter(1))
+                cost_per_road.append(int(temp[1]))
+                road_path.append(temp[0])
+
+            sifis += 1
+
+        total_cost = sum(cost_per_road)
+
+        return total_cost, cost_per_road, road_path
+
+
+    def populate_graph(self, road, node1, node2, cost):
+
+        if not(node1 in self.graph_dict):
+
+            self.graph_dict[node1] = {}
+
+        if not(node2 in self.graph_dict[node1]):
+
+            self.graph_dict[node1][node2] = {}
+
+        if not(node2 in self.graph_dict):
+
+            self.graph_dict[node2] = {}
+
+        if not(node1 in self.graph_dict[node2]):
+
+            self.graph_dict[node2][node1] = {}
+
+        self.graph_dict[node1][node2][road] = cost
+        self.graph_dict[node2][node1][road] = cost
+
+
+    def breadth_first_search(self, source, destination):
+
+        node = source
+        fringe = list()
+        visited = list()
+
+        fringe.append(node)
+        while fringe:
+
+            node = fringe.pop(0)
+            visited.append(node)
+
+            if node == destination:
+
+                return visited
+
+            for child in self.graph_dict[node]:
+
+                if (child not in visited) and (child not in fringe):
+
+                    fringe.append(child)
+
+
+    def heuristic(self, node):
+
+        return 38
+
+
+    def ida_star(self, predicted_traffic, source, destination):
+
+        limit = self.heuristic(source)
+        path = [source]
+        path_of_roads = []
+        cost_of_path = []
+
+        while True:
+
+            t = self.search(predicted_traffic, source, destination, path_of_roads, cost_of_path, path, 0, limit)
+            if t == -1:
+                return path, path_of_roads, cost_of_path, limit
+            
+
+            limit = t
+
+
+    def search(self, predicted_traffic, source, destination, path_of_roads, cost_of_path, path, g, limit):
+
+        node = path[-1]
+
+        f = g + self.heuristic(node)
+
+        if f > limit:
+            return f
+
+        if node == destination:
+            return -1
+
+        min = float("inf")
+        successors, succ_roads, succ_road_cost = self.sorted_successors(predicted_traffic, node)
+        counter = 0;
+        for child in successors:
+
+            if child not in path:
+
+                step_cost = succ_road_cost[counter] - self.heuristic(node)
+
+                path.append(child)
+                path_of_roads.append(succ_roads[counter])
+                cost_of_path.append(step_cost)
+                # step_cost, step_road = self.min_road_cost(predicted_traffic, node, child)
+                
+                t = self.search(predicted_traffic, source, destination, path_of_roads, cost_of_path, path, g + step_cost, limit)
+                if t == -1:
+                    return -1
+                if t < min:
+                    min = t
+
+                path.pop()
+                path_of_roads.pop()
+                cost_of_path.pop()
+
+                counter += 1
+
+        return min
+
+
+    def min_road_cost(self, predicted_traffic, node, child):
+
+        min_cost = float("inf")
+
+        for road, cost in self.graph_dict[node][child].items():
+
+            traffic_status = predicted_traffic[road]
+
+            if(traffic_status == "low"):
+
+                temp_cost = int(cost)*0.9 
+
+            elif(traffic_status == "normal"):
+
+                temp_cost = int(cost) 
+
+            else:
+
+                temp_cost = int(cost)*1.25
+
+            if temp_cost < min_cost:
+
+                min_cost = temp_cost
+                min_road = road
+
+        return min_cost, min_road
+
+
+    def sorted_successors(self, predicted_traffic, node):
+
+        cheapest_path = []
+        cheapest_road = []
+        cheapest_road_cost = []
+
+        for child in self.graph_dict[node]:
+
+            temp0_cost, temp_road = self.min_road_cost(predicted_traffic, node, child)
+            # Sort the successors by calculating f(n), since all children have the same father, only the step cost and h(n) change.
+            temp_cost = temp0_cost + self.heuristic(child)
+
+            if not cheapest_road_cost:
+
+                cheapest_road_cost.append(temp_cost)
+                cheapest_road.append(temp_road)
+                cheapest_path.append(child)
+
+            else:
+                
+                x = bisect.bisect_left(cheapest_road_cost, temp_cost)
+
+                if x == len(cheapest_road_cost):
+                    cheapest_road_cost.append(temp_cost)
+                    cheapest_road.append(temp_road)
+                    cheapest_path.append(child)
+                else:
+                    cheapest_road_cost.insert(x, temp_cost)
+                    cheapest_road.insert(x, temp_road)
+                    cheapest_path.insert(x, child)
+
+
+        return cheapest_path, cheapest_road, cheapest_road_cost
+
+               
+             
---
+++ main.py
@@ -0,0 +1,146 @@
+
+import Graph
+import pprint
+import linecache
+import time
+
+
+graph_dict = {}
+traffic = {}
+
+
+def remove_tags(text):
+
+    import re
+    clean = re.compile('<.*?>')
+    return re.sub(clean, '', text)
+
+
+def read_traffic(traffic_line, road_number, lines):
+
+    road_counter = 1
+
+    while road_counter <= road_number:
+
+        if(lines[traffic_line] == "<Day>\n" or lines[traffic_line] == "</Day>\n"):
+
+            traffic_line += 1
+
+        else:
+
+            line = lines[traffic_line]
+            temp = line.split(';' ' ', 2)
+            traffic[temp[0]] = temp[1].rstrip()
+            road_counter += 1
+            traffic_line += 1
+
+    return traffic, traffic_line
+
+
+def calculate_real_cost(road_path, predicted_cost_per_road):
+
+    actual_total_cost = 0
+    index = 0
+
+    for road in road_path:
+
+        traffic_status = traffic[road]
+
+        if(traffic_status == "low"):
+
+            actual_total_cost += predicted_cost_per_road[index]*0.9
+
+        elif(traffic_status == "normal"):
+
+            actual_total_cost += predicted_cost_per_road[index]
+
+        else:
+
+            actual_total_cost += predicted_cost_per_road[index]*1.25
+
+        index += 1
+
+    return actual_total_cost
+
+
+def output(path, total_cost, cost_per_road, actual_total_cost, algo_name, day):
+
+    number_of_nodes = len(path)
+
+    if algo_name != "IDA*":
+        print("Day", day)
+    
+    print(algo_name+":")
+    print("\tVisited Nodes number:", number_of_nodes)
+    print("\tPath:", end='')
+
+    index = 0
+    for index in range(len(path)):
+
+        if index == len(path)-1:
+
+            print(path[index], end='')
+
+        else:
+
+            print(path[index]+"("+str(cost_per_road[index])+")"+"->", end='')
+
+        index += 1
+
+    print("")
+    print("\tPredicted cost:", total_cost)
+    print("\tReal Cost:", actual_total_cost)
+
+
+def main():
+
+    p1 = 0.6
+    p2 = 0.2
+    p3 = 0.2
+
+    graph = Graph.Graph(graph_dict, p1, p2, p3)
+
+    file = open("sampleGraph1.txt", "r")
+
+    source, destination, actual_traffic_line, predictions_line, roads_count, average_cost_per_road = graph.read_graph(file)
+
+    file.close()
+
+    source = source.rstrip()
+    destination = destination.rstrip()
+
+    path_bfs = graph.breadth_first_search(source, destination)
+
+    predicted_total_cost, predicted_cost_per_road, road_path = graph.calculate_cost_bfs(path_bfs)
+
+
+    file = open("sampleGraph1.txt", "r")
+    lines = file.readlines()
+
+    days = 80
+    road_number = roads_count
+    a_traffic_line = actual_traffic_line + 1
+    p_traffic_line = predictions_line + 1
+
+    
+    for current_day in range(days):
+
+        actual_traffic, a_traffic_line = read_traffic(a_traffic_line, road_number, lines)
+
+        actual_real_cost_bfs = calculate_real_cost(road_path, predicted_cost_per_road)
+
+        predicted_traffic, p_traffic_line = read_traffic(p_traffic_line, road_number, lines)
+
+        path_ida, path_of_roads, cost_of_path, limit = graph.ida_star(predicted_traffic, source, destination)
+
+        actual_real_cost_ida = calculate_real_cost(path_of_roads, cost_of_path)
+
+        output(path_bfs, predicted_total_cost, predicted_cost_per_road, actual_real_cost_bfs, "Breadth First Search", current_day + 1)
+        output(path_ida, sum(cost_of_path), cost_of_path, actual_real_cost_ida, "IDA*", current_day + 1)
+
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    main()
+    print("--- %s seconds ---" % (time.time() - start_time))
